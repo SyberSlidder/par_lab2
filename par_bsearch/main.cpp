690e6067d4c78f2e3e4e526978c4d997d9d60696
syberslidder <syberslidder@gmail.com>
2015-03-08 16:14:27 -0500

    MPI changes

--- par_bsearch/main.cpp
+++ par_bsearch/main.cpp
@@ -5,6 +5,9 @@
 #include <strings.h>
 #include <mpi.h>
 #include <limits.h>
+#include <omp.h>
+
+#define DEBUG 1
 
 void print_array(int * ptr, int num_elements) {
   for(int i = 0; i < num_elements; i++) {
@@ -35,10 +38,12 @@ void* par_bsearch (
 	    size_t size,
 	    int (*compar)(const void*,const void*),
 	    int num_keys,
-	    int mpi_comm_rank,
+	    MPI_Comm mpi_comm,
 	    int num_threads
 	    )
 {
+  
+  
   
   return (void *)0;
   
@@ -47,26 +52,136 @@ void* par_bsearch (
 
 void search(int num_elements) {
 
-  int * int_array = new int[num_elements];
-  
-  srand(time(NULL));
-  // initialize to random integers
-  for (int i = 0; i < num_elements; i++) {
-    int_array[i] = (rand() % INT_MAX) + 1;
-  }
-  // Sort the array
-  //print_array(int_array,num_elements);
-  //printf("\n");
-  qsort((void *) int_array,num_elements,sizeof(int),int_comparator);
 
-  //print_array(int_array,num_elements);
+
   
 }
   
 int main(int argc, char * argv[]) {
   
-  search(5);
+   int array_size;
+   int nodes;
+   int sockets_per_node;
+   int threads_per_socket;
+   int num_keys;
+   
+   if (argc < 6) {
+     printf("Usage: ./bsearch array_size num_keys nodes sockets_per_node threads_per_socket\n");
+     return -1;
+   } else {
+      
+      sscanf(argv[1],"%d",&array_size);
+      sscanf(argv[2],"%d",&num_keys);
+      sscanf(argv[3],"%d",&nodes);
+      sscanf(argv[4],"%d",&sockets_per_node);
+      sscanf(argv[5],"%d",&threads_per_socket);
+      /*
+      printf("Elements: %d\n",array_size);
+      printf("Nodes: %d\n",nodes);
+      printf("Sock/Node: %d\n",sockets_per_node);
+      printf("Thread/Sock: %d\n",threads_per_socket);
+      */
+   }
   
+    MPI_Init(&argc,&argv);
+    
+    int rank;
+    int size;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    int elems_per_task = array_size / size;
+    int remainder = array_size % elems_per_task;
+
+    // P0 is the master process
+    int * int_array = 0;
+    int * keys_array = 0;
+    int startIndex = 0;
+    int endIndex = 0;
+    MPI_Status status_handle;
+    
+    if (rank == 0) {
+      srand(time(NULL));
+      
+      int_array  = new int[array_size];
+      keys_array = new int[num_keys]; 
+      
+      // Place keeper
+      for (int i = 0; i < num_keys; i++) {
+	keys_array[i] = (rand() % INT_MAX) + 1;
+      }
+      
+      // initialize to random integers
+      for (int i = 0; i < array_size; i++) {
+	int_array[i] = (rand() % INT_MAX) + 1;
+      }
+      // Sort the array
+      //printf("Raw array: \n");
+      //print_array(int_array,array_size);
+      //printf("\n");
+      qsort((void *) int_array,array_size,sizeof(int),int_comparator);
+      //printf("Sorted array: \n");
+      //print_array(int_array,array_size);
+      //printf("\n");
+      
+      startIndex = 0;
+      endIndex   = startIndex + (elems_per_task) + remainder;
+      int * send_buff = int_array + endIndex;
+      
+      // Send out parts of the sorted array
+      for (int curr_rank = 1; curr_rank < size; curr_rank++) {
+	MPI_Ssend(send_buff,elems_per_task,MPI_INT,curr_rank,0,MPI_COMM_WORLD);
+	send_buff += elems_per_task;
+      }
+      
+      // Send out the keys we are looking for
+      for (int curr_rank = 1; curr_rank < size; curr_rank++) {
+	MPI_Ssend(keys_array,num_keys,MPI_INT,curr_rank,1,MPI_COMM_WORLD);
+      }     
+      
+    } else {
+      
+      int_array = new int[elems_per_task];
+      keys_array = new int[num_keys];
+      
+      MPI_Recv((void* )int_array, elems_per_task, MPI_INT, 0, 0, MPI_COMM_WORLD,&status_handle);
+      
+      startIndex = remainder + elems_per_task*rank;
+      endIndex = startIndex + elems_per_task;
+      
+      MPI_Recv((void *)keys_array, num_keys, MPI_INT, 0, 1, MPI_COMM_WORLD, &status_handle);
+      
+    }
+    
+    // At this point the array is distributed between MPI Tasks and all know which keys 
+    // they are looking for.
+    
+    // Some openmp book keeping
+    omp_set_num_threads(threads_per_socket);
+    
+    /*
+    printf("Array from Rank %d of size %d: ",rank,size);
+    for (int i = 0; i < elems_per_task; i++) {
+      printf("%d ",int_array[i]);
+    }
+    printf("\n");
+    */
+    
+    /*
+    omp_set_num_threads(6);
+    
+    int threadID;
+    
+    #pragma omp parallel private(threadID)
+    {
+      threadID = omp_get_thread_num();
+      printf("Thread: %d of Rank: %d of Size: %d\n",threadID,rank,size);
+  
+    }
+    */
+    MPI_Finalize();
+    
     return 0;
   
 }
